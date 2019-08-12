f514f950af15fffaf76b15856c5ad2505216a607
riy0 <chac0ptx@gmail.com>
2019-08-12 19:32:34 +0900

    good and comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def create
+    @comment = Comment.new(comment_params)
+    @post = @comment.post
+    if @comment.save
+      respond_to :js
+    else
+      flash[:alert]="fail to comment"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @post = @comment.post
+    if @comment.destroy
+      respond_to :js
+    else
+      flash[:alert] = "fail to delete comment"
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:user_id, :post_id, :comment)
+    end
+end

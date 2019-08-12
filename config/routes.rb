ae4b958125bd222b5485df4570e0ebccbcff8613
riy0 <chac0ptx@gmail.com>
2019-08-12 20:28:51 +0900

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
+#  get '/users/:id', to: 'users#show', as: 'user'
 Rails.application.routes.draw do
-  devise_for :users,
-    controllers: { registrations: 'registrations' }
-
   root 'posts#index'
 
-  get '/users/:id', to: 'users#show', as: 'user'
+  devise_for :users,
+  controllers: { registrations: 'registrations' }
+
+  resources :users, only: %i(show)
 
   resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
-    resources :likes, only: %i(create destroy)
-    resources :comments, only: %i(create destroy)
-
+    resources :likes, only: %i(create destroy), shallow: true
+    resources :comments, only: %i(index create destroy), shallow: true
   end
 end

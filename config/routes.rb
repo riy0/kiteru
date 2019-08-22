6f81cdd5949d92d30162cc3ae22a609858ad1df3
riy0 <chac0ptx@gmail.com>
2019-08-22 15:27:49 +0900

    add logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    controllers: { registrations: 'registrations' }
-
   root 'posts#index'
 
-  get '/users/:id', to: 'users#show', as: 'user'
+  devise_for :users,
+  controllers: { registrations: 'registrations' }
+
+  resources :users, only: %i(show)
 
   resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
-    resources :likes, only: %i(create destroy)
-    resources :comments, only: %i(create destroy)
-
+    resources :likes, only: %i(create destroy), shallow: true
+    resources :comments, only: %i(index create destroy), shallow: true
   end
 end
+
+#  get '/users/:id', to: 'users#show', as: 'user'
